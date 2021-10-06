b76f343d59bde19fd580a1b788a9cb0e87e631e0
Андрей Британчук <avbritanchuk@mail.ru>
2021-10-06 15:39:47 +0500

    2

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -33,25 +33,27 @@ namespace pis
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(487, 449);
+            this.dataGridView1.Size = new System.Drawing.Size(1129, 449);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(577, 53);
+            this.button1.Location = new System.Drawing.Point(1149, 22);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 59);
             this.button1.TabIndex = 1;
@@ -62,7 +64,7 @@ namespace pis
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button2.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(577, 167);
+            this.button2.Location = new System.Drawing.Point(1149, 113);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(161, 59);
             this.button2.TabIndex = 2;
@@ -73,18 +75,30 @@ namespace pis
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button3.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(577, 282);
+            this.button3.Location = new System.Drawing.Point(1149, 209);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(161, 59);
             this.button3.TabIndex = 3;
             this.button3.Text = "Удалить";
             this.button3.UseVisualStyleBackColor = false;
             // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(1149, 306);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 59);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Добавить";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1322, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -103,5 +117,6 @@ namespace pis
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -21,12 +21,23 @@ namespace pis
         {
             dataGridView1.RowHeadersVisible = false;
             dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.RowCount = 3;
-            dataGridView1.ColumnCount = 3;
-            dataGridView1.Rows[0].Cells[0].Value = Convert.ToString("Акт:");
-            dataGridView1.Rows[0].Cells[1].Value = Convert.ToString("Город:");
-            dataGridView1.Rows[0].Cells[2].Value = Convert.ToString("Отделение:");
             
+            dataGridView1.RowCount = 3;
+            dataGridView1.ColumnCount = 12;
+            dataGridView1.Rows[0].Cells[0].Value = Convert.ToString("Номер МК");
+            dataGridView1.Rows[0].Cells[1].Value = Convert.ToString("Дата заключения МК");
+            dataGridView1.Rows[0].Cells[2].Value = Convert.ToString("Муниципальное образование");
+            dataGridView1.Rows[0].Cells[3].Value = Convert.ToString("ОМ СУ");
+            dataGridView1.Rows[0].Cells[4].Value = Convert.ToString("Исполнитель МК");
+            dataGridView1.Rows[0].Cells[5].Value = Convert.ToString("Номер акта отлова");
+            dataGridView1.Rows[0].Cells[6].Value = Convert.ToString("Количество отловленных собак");
+            dataGridView1.Rows[0].Cells[7].Value = Convert.ToString("Количество отловленных кошек");
+            dataGridView1.Rows[0].Cells[8].Value = Convert.ToString("Количество отловленных животных");
+            dataGridView1.Rows[0].Cells[9].Value = Convert.ToString("Населенный пункт");
+            dataGridView1.Rows[0].Cells[10].Value = Convert.ToString("Дата отлова");
+            dataGridView1.Rows[0].Cells[11].Value = Convert.ToString("Цель отлова");
+
+
 
             dataGridView1.Rows[1].Cells[0].Value = Convert.ToString("Акт 2");
             dataGridView1.Rows[1].Cells[1].Value = Convert.ToString("г. Пермь");
