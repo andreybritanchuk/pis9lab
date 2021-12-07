37ae0d796b190249951732b499f7e9aad5dabcfb
Андрей Британчук <avbritanchuk@mail.ru>
2021-12-07 15:32:12 +0500

    7 Lab

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis
+{
+    class Controller
+    {
+        public void choiseElement(int[] idCard)
+        {
+
+        }
+
+        public void choiseElementForWord(int idCard)
+        {
+
+        }
+
+        public void openCard(int idCard)
+        {
+
+        }
+
+        public void openRegistry()
+        {
+
+        }
+
+        public object[] openRegistry(string filter, string sorter)
+        {
+            return null;
+        }
+
+
+    }
+}
